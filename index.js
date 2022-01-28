027dccfa81b41f5c8ab605d8c8a029b4293b49d7
Vikas Nautiyal <vikas.nautiyal@comprotechnologies.com>
2022-01-28 09:45:02 +0530

    Format log processing method

--- index.js
+++ index.js
@@ -11,57 +11,74 @@ const s3 = new AWS.S3();
 const regexp = /[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}Z/;
 
 exports.handler = async (event, context) => {
+  try {
     const accountOwner = JSON.stringify(context.invokedFunctionArn).split(':')[4];
 
     for (const { messageId, body } of event.Records) {
-        const awsLogs  = {
-          messageType: "DATA_MESSAGE",
-          owner: accountOwner,
-          logEvents:[]
-        };
-        
-        const message = body.Records[0];
-        if(!message.eventSource == "aws:s3"){
-          return;
-        }
-        const bucket = message.s3.bucket.name;
-        const file = message.s3.object.key;
-       
-        if(path.extname(file) == ".gz"){
-          const pathlevels = file.split("/");
+      const message = body.Records[0];
+      if (message.eventSource != "aws:s3") {
+        return;
+      }
 
-          awsLogs.logStream = pathlevels[pathlevels.length - 2].replace(/\//g, "-");
+      const file = message.s3.object.key;
+      if(path.extname(file) != ".gz") {
+        return;
+      }
 
-          const taskId = pathlevels[pathlevels.length - 3];
-          const taskdetails = await cloudwatchlogs.describeExportTasks({taskId}).promise();
-          awsLogs.logGroup = taskdetails.exportTasks[0].logGroupName;
+      const awsLogs = await getLogParams(message, file, accountOwner);
 
-          const s3ReadStream = s3.getObject({
-              Bucket: bucket,
-              Key: file
-          }).createReadStream().pipe(zlib.createGunzip());
+      const bucket = message.s3.bucket.name;
+      const s3ReadStream = s3.getObject({
+          Bucket: bucket,
+          Key: file
+      }).createReadStream().pipe(zlib.createGunzip());
 
-          const rl = readline.createInterface({
-            input: s3ReadStream,
-            terminal: false
-          });
+      const rl = readline.createInterface({
+        input: s3ReadStream,
+        terminal: false
+      });
 
-          for await (const currentline of rl) {
-            if (currentline.length > 0) {
-              const result = currentline.match(regexp);
-              if(result){
-                awsLogs.logEvents.push({
-                  "timestamp": new Date(result[0]).getTime(),
-                  "message": currentline.substring(result[0].length + 1)
-                })
-              }
-            }
+      for await (const currentline of rl) {
+        if (currentline.length > 0) {
+          const result = currentline.match(regexp);
+          if (result) {
+            awsLogs.logEvents.push({
+              "timestamp": new Date(result[0]).getTime(),
+              "message": currentline.substring(result[0].length + 1)
+            })
           }
-
-          var bufferObject = new Buffer.from(JSON.stringify(awsLogs));
-          const gz = zlib.gzipSync(bufferObject).toString('base64')
-          console.log(gz)
         }
+      }
+
+      gzipLogs(awsLogs);
     }
     return `Successfully processed ${event.Records.length} messages.`;
+  } catch (error) {
+    console.log("Error: ", error);
+  }
 };
+
+async function getLogParams(message, file, accountOwner) {
+  const awsLogs = {
+    messageType: "DATA_MESSAGE",
+    owner: accountOwner,
+    logGroup: null,
+    logStream: null,
+    logEvents:[]
+  };
+
+  const pathlevels = file.split("/");
+  const taskId = pathlevels[pathlevels.length - 3];
+  const taskdetails = await cloudwatchlogs.describeExportTasks({taskId}).promise();
+  awsLogs.logGroup = taskdetails.exportTasks[0].logGroupName;
+  awsLogs.logStream = pathlevels[pathlevels.length - 2].replace(/\//g, "-");
+
+  return awsLogs;
+}
+
+function gzipLogs(awsLogs) {
+  var bufferObject = new Buffer.from(JSON.stringify(awsLogs));
+  const gz = zlib.gzipSync(bufferObject).toString('base64')
+  console.log(gz)
+  return gz;
+}
